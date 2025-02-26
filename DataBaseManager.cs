374ad869621cc94c83cf015d78bb729d0975b762
Bergamot <Bergamot@BRGMT>
2025-02-27 00:16:01 +0200

    Добавьте файлы проекта.

---
+++ DataBaseManager.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace OnlineStoreApp_HomeWork
+{
+    internal class DataBaseManager
+    {
+        private readonly string connectionString = "Server=localhost;Database=OnlineStore;Trusted_Connection=True;Encrypt=False;";
+
+
+        public bool TestConnection()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    Console.WriteLine("Coonecting to server succesfully");
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка подключения: {ex.Message}");
+                return false;
+            }
+        }
+        public void AddProduct(string name, string description, decimal price, int stock)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Products (Name, Description, Price, Stock) VALUES (@name, @description, @price, @stock)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@description", description);
+                    command.Parameters.AddWithValue("@price", price);
+                    command.Parameters.AddWithValue("@stock", stock);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void AddCategory (string categoryName)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Categories (CategoryName) VALUES (@CategoryName)";
+                if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@CategoryName", categoryName);
+
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                        Console.WriteLine("Category added successfully.");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                    }
+                    finally
+                    {
+                        
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+        public void LinkProductToCategory(int productId, int categoryId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO ProductCategories (ProductId, CategoryId) VALUES (@productId, @categoryId)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@productId", productId);
+                    command.Parameters.AddWithValue("@categoryId", categoryId);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+
+            }
+
+
+        }
+        public void GetAllProducts()
+        {
+            using(SqlConnection connection = new SqlConnection (connectionString))
+            {
+                string query = "SELECT * FROM Products";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        Console.WriteLine("\nProduct List:");
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"ID: {reader["ProductID"]}, Name: {reader["Name"]}, Description: {reader["Description"]}, Price: {reader["Price"]}, Stock: {reader["Stock"]}");
+                        }
+                        Console.WriteLine();
+                    }
+                            
+                        
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿namespace OnlineStoreApp_HomeWork
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            //Console.WriteLine("Hello, World!");
+
+
+            DataBaseManager dbManager = new DataBaseManager();
+            if (!dbManager.TestConnection())
+            {
+                Console.WriteLine("No connection");
+                return;
+            }
+            while (true)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Add Product");
+                Console.WriteLine("2. Add Category");
+                Console.WriteLine("3. Connect product to category");
+                Console.WriteLine("4. Show All Products");
+                Console.WriteLine("0. Exit");
+
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Введите название продукта: ");
+                        string? name = Console.ReadLine();
+
+                        Console.Write("Введите описание продукта: ");
+                        string? description = Console.ReadLine();
+
+                        Console.Write("Введите цену продукта: ");
+                        if (!decimal.TryParse(Console.ReadLine(), out decimal price))
+                        {
+                            Console.WriteLine("Ошибка: Некорректный ввод цены!\n");
+                            break;
+                        }
+
+                        Console.Write("Введите количество на складе: ");
+                        if (!int.TryParse(Console.ReadLine(), out int stock))
+                        {
+                            Console.WriteLine("Ошибка: Некорректный ввод количества!\n");
+                            break;
+                        }
+
+                        if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(description))
+                        {
+                            Console.WriteLine("Ошибка: Название и описание не могут быть пустыми!\n");
+                            break;
+                        }
+
+                        dbManager.AddProduct(name, description, price, stock);
+                        Console.WriteLine("Продукт добавлен!\n");
+                        break;
+
+                    
+
+                    case "2":
+                        Console.Write("Введите название категории: ");
+                        string? categoryName = Console.ReadLine();
+
+                        if (string.IsNullOrWhiteSpace(categoryName))
+                        {
+                            Console.WriteLine("Ошибка: Название категории не может быть пустым!\n");
+                            break;
+                        }
+
+                        dbManager.AddCategory(categoryName);
+                        Console.WriteLine("Категория добавлена!\n");
+                        break;
+
+                    case "3":
+                        Console.Write("Введите ID продукта: ");
+                        if (!int.TryParse(Console.ReadLine(), out int productId))
+                        {
+                            Console.WriteLine("Ошибка: Некорректный ввод ID продукта!\n");
+                            break;
+                        }
+
+                        Console.Write("Введите ID категории: ");
+                        if (!int.TryParse(Console.ReadLine(), out int categoryId))
+                        {
+                            Console.WriteLine("Ошибка: Некорректный ввод ID категории!\n");
+                            break;
+                        }
+
+                        dbManager.LinkProductToCategory(productId, categoryId);
+                        Console.WriteLine("Продукт привязан к категории!\n");
+                        break;
+                    case "4":
+                        dbManager.GetAllProducts();
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.WriteLine("Wrong number. Try again.\n");
+                        break;
+                }
+            }
+        }
+    }
+}
